7cb6501d0f103e539aad118911d8011b74e5c649
shegxino <46869815+shegxino@users.noreply.github.com>
2020-10-03 22:32:06 -1200

    Add files via upload

---
+++ aweby.js
@@ -0,0 +1,41 @@
+// Create a function for mobile version
+(function( $ ){
+	$.fn.grtmobile = function () {if ($(window).width() < 768) {
+$('.grt-mobile-button').on('click', function(){
+$(this).toggleClass("grt-mobile-button-open");
+$("ul.grt-menu").toggleClass("open-grt-menu ");
+$("html, body").toggleClass("body-overflow");
+});
+$('li.grt-dropdown').on('click', function(e){
+$(this).toggleClass("active-dropdown");
+});
+}
+}
+})( jQuery );
+
+// Initialize and check for mobile
+$.fn.grtmobile();
+
+// On resize window check for mobile
+var resizeTimer;
+
+$(window).on('resize', function(e) {
+clearTimeout(resizeTimer);
+resizeTimer = setTimeout(function() {
+$.fn.grtmobile();
+}, 250);
+})
+
+// Add shadow on scroll after 60px
+$(window).scroll(function(e){if ($(this).scrollTop() > 60){
+$('header').addClass('scrolled');
+}
+else {
+$('header').removeClass('scrolled');
+}
+});
+
+// Prevent a href clicks on dropdown category
+$('li.grt-dropdown > a').on('click', function(e){
+e.preventDefault();
+});
